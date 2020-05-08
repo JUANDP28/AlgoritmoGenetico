f9028a1a9d0ada7f1fa972f7ec7b5727fa5bf83b
JUANDP28 <48769717+JUANDP28@users.noreply.github.com>
2020-05-07 23:55:21 -0500

    Creación de Proyecto y Generación de la población
    Se creo el proyecto en C# y se agrego la funcionalidad de generar la población
    
    Co-Authored-By: mariajosehm99 <mariajosehm99@users.noreply.github.com>

---
+++ AlgoritmoGenetico/Individuo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgoritmoGenetico {
+    class Individuo {
+
+        private List<Double> elementos = new List<double>();
+        private Double aptitud;
+
+        public Individuo () {
+
+            for (int i = 0; i < 10; i++) {
+                elementos.Add(GenerarValor());
+            }
+
+            this.aptitud = CalcularAptitud();
+        }
+
+        public Double GenerarValor () {
+            Random random= new Random();
+            int valorEntero = random.Next(-5120, 5121);
+            return valorEntero / 1000.0;
+        }
+
+        public Double CalcularAptitud () {
+            Double suma = 0;
+            foreach (Double elemento in elementos) {
+                suma += (Math.Pow(elemento, 2) - (10 * Math.Cos(2 * Math.PI * elemento)));
+            }
+            return Math.Round((100 + suma), 3);
+        }
+
+        public Double GetAptitud () {
+            return this.aptitud;
+        }
+    }
+}
---
+++ AlgoritmoGenetico/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgoritmoGenetico {
+    class Program {
+
+        List<Individuo> poblacion = new List<Individuo>();
+
+        public void GenerarPoblacion () {
+            Console.WriteLine("");
+            Console.WriteLine("=============== GENERANDO POBLACION ===============");
+            Console.WriteLine("");
+            for (int i = 0; i < 100; i++) {
+                poblacion.Add(new Individuo());
+            }
+            Console.WriteLine("");
+            Console.WriteLine("=============== POBLACION GENERADA ===============");
+            Console.WriteLine("");
+        }
+
+        public void ImprimirAptitud () {
+            foreach (Individuo elemento in poblacion) {
+                Console.WriteLine("APTITUD: " + elemento.GetAptitud());
+            }
+        }
+        static void Main (string [] args) {
+
+            Program algoritmo = new Program();
+            algoritmo.GenerarPoblacion();
+            algoritmo.ImprimirAptitud();
+            
+        }
+    }
+}
